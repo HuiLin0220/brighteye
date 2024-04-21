9ba5fb8c53bfa226a41c3a6d53f2d4b5fa9d331b
hui lin <62810108+HuiLin0220@users.noreply.github.com>
2024-04-21 12:50:17 -0500

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,20 +1,14 @@
 import numpy
 from PIL import Image
 from helper import DEFAULT_GLAUCOMATOUS_FEATURES, inference_tasks
-
-
-from timm.models import create_model
 import torch
-from torch.autograd import Variable
-import torchvision.transforms as transforms
-from models.MIL_VT import*
 import utils
+from ultralytics import YOLO
+from utils import *
 
 
-img_size = 512
-C,H,W = 3,img_size,img_size
-weights_path = "./weights/"
-features = ['ANRS','ANRI','RNFLDS',
+gpu_id = '0'
+feature_name = ['ANRS','ANRI','RNFLDS',
  'RNFLDI',
  'BCLVS',
  'BCLVI',
@@ -22,65 +16,61 @@ features = ['ANRS','ANRI','RNFLDS',
  'DH',
  'LD',
  'LC']
-device = torch.device("cuda:0")
+ 
+weights_path = "./weights/"
+#model path
+cls_center_model_weight = weights_path + "/cls_center.pt" 
 
-
-
-bi_cls_PATH = weights_path + "binary_cls.pth.tar"
 model_path_list = []
-for idx, f in enumerate(features):
-    MODEL_PATH = weights_path + str(idx) + f + ".pth.tar"
+for idx, f in enumerate(feature_name):
+    MODEL_PATH = weights_path + str(idx) + f + '.pt'
     model_path_list.append(MODEL_PATH)
+#print(model_path_list)
 
 
-transform_test = transforms.Compose([
-        transforms.Resize((img_size, img_size)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
-    ])
 
-     
 def run():
-    _show_torch_cuda_info()
-    bi_cls_model = loadmodel(bi_cls_PATH)
-    
+    #_show_torch_cuda_info()
+    #load model
+    cls_model = YOLO(cls_center_model_weight)
+
     multi_cls_models = []
     for i in range(10):
-      model = loadmodel(model_path_list[i])
+      model = YOLO(model_path_list[i])
       multi_cls_models.append(model)
-    
-     
+
 
     for jpg_image_file_name, save_prediction in inference_tasks():
         # Do inference, possibly something better performant
         ...
 
-        print(f"Running inference on {jpg_image_file_name}")
+        print(f"Running inference on {jpg_image_file_name}")  
 
+        cropped_image = crop_center(jpg_image_file_name)
         
-        Img = Image.open(jpg_image_file_name)
-        inputs = transform_test(Img)
-        inputs = torch.reshape(inputs, (1,C,H,W))
-        inputs = inputs.to(device)
         
-      
-
-        is_referable_glaucoma_likelihood = binary_prediction(bi_cls_model,inputs)  
+        results = cls_model.predict(cropped_image,device = gpu_id,imgsz=512)
+        result=results[0]
+        is_referable_glaucoma_likelihood = result.probs.data.cpu().numpy()[1]
+        is_referable_glaucoma_likelihood = is_referable_glaucoma_likelihood.astype(numpy.float64)
+        
+          
         is_referable_glaucoma = bool(is_referable_glaucoma_likelihood > 0.5)
-        
         
         
         if True:
         
-            feature_pred = feature_prediction(multi_cls_models,inputs)
+            feature_pred = feature_prediction(multi_cls_models,cropped_image,gpu_id)
 
             features = {}
             for idx, (k, v) in enumerate(DEFAULT_GLAUCOMATOUS_FEATURES.items()):
-              features[k] =  bool(feature_pred[idx]> 0.5)  
+              features[k] =  bool(feature_pred[idx]>= 0.5)
             
         else:
             features = None
-        print(features)
+        
+        ...
+
         # Finally, save the answer
         save_prediction(
             is_referable_glaucoma,
@@ -104,48 +94,16 @@ def _show_torch_cuda_info():
         print(f"\tproperties: {torch.cuda.get_device_properties(current_device)}")
     print("=+=" * 10)
 
-
-def loadmodel(MODEL_PATH):
-    #print(MODEL_PATH)
-    model = create_model(model_name='MIL_VT_small_patch16_512',
-            pretrained=False,
-            num_classes=2,
-            drop_rate=0,
-            drop_path_rate=0.1,
-            drop_block_rate=None
-            )
-    checkpoint0 = torch.load(MODEL_PATH, map_location='cpu')
-    model.load_state_dict(checkpoint0['state_dict'])
     
-
-    return model
-
-def binary_prediction(model,inputs):
-
-    model.eval()
-    bi_cls_model = model.to(device)
-    inputs = inputs.to(device)
-    outputs_class = model(inputs)
-
-    is_referable_glaucoma_likelihood = utils.softmax(outputs_class[0].data.cpu().numpy()) 
-    
-    is_referable_glaucoma_likelihood = is_referable_glaucoma_likelihood[1]
-    is_referable_glaucoma_likelihood = is_referable_glaucoma_likelihood.astype(numpy.float64) 
-    
-    return is_referable_glaucoma_likelihood
-    
-def feature_prediction(models,inputs):
+def feature_prediction(models,cropped_image,gpu_id):
     predictions_class=[]
     for idx, model in enumerate(models):
-            
-            model.eval()
-            model = model.to(device)
-            outputs_class = model(inputs)
-            outputs_class = utils.softmax(outputs_class.data.cpu().numpy())
-            
-            outputs_class = numpy.asarray(outputs_class)
-            preds = numpy.argmax(outputs_class)
-            predictions_class.append(preds)
+       
+        results = model.predict(cropped_image,device = gpu_id,imgsz=640)
+        result=results[0]
+        preds = result.probs.data.cpu().numpy()[1]
+         
+        predictions_class.append(preds)
 
     return predictions_class
 
